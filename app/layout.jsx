453cca4425488499eee3a8e3405869db37e4bc61
karthik-m96 <karthikm300796@gmail.com>
2023-06-25 12:27:26 +0530

    styling

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+const metadata = {
+  title: "promptopia",
+  description: "Discover and Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
